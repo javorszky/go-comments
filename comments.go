bec9dd0a9d4cc201a92217e4122e92fe8fc5e9d8
Gabor Javorszky <gabor@javorszky.co.uk>
2019-01-16 14:13:11 +0000

    Start the server on port

--- comments.go
+++ comments.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo"
+	"os"
 )
 
 func main() {
 	fmt.Println("hello world")
 
 	e := echo.New()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		fmt.Print("Port not in env, setting it to 8090")
+		port = "8090"
+	}
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
