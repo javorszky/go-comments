8d08892497b8d7fd25cdd8712fd7e7d7b0b7ef87
Gabor Javorszky <gabor@javorszky.co.uk>
2019-01-30 15:53:17 +0000

    Add tests for routes

--- handlers_test.go
+++ handlers_test.go
@@ -5,11 +5,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 var (
-	serveJS = `console.log("The id of the thing requesting this was 44");`
+	serveJS        = `console.log("The id of the thing requesting this was 44");`
+	mockUser       = `{"email":"test@example.com","name":"John Doe","password":"somepassword"}`
+	mockUserReturn = `{"ID":0,"CreatedAt":"0001-01-01T00:00:00Z","UpdatedAt":"0001-01-01T00:00:00Z","DeletedAt":null,"email":"test@example.com","name":"John Doe","password":"somepassword"}`
 )
 
 func TestServeJS(t *testing.T) {
@@ -27,5 +30,67 @@ func TestServeJS(t *testing.T) {
 	if assert.NoError(t, ServeJS(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, serveJS, rec.Body.String())
+	}
+}
+
+func TestIndex(t *testing.T) {
+	e := echo.New()
+	SetRenderer(e)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	c.SetPath("/")
+
+	if assert.NoError(t, Index(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+func TestLogin(t *testing.T) {
+	e := echo.New()
+	SetRenderer(e)
+
+	req := httptest.NewRequest(http.MethodGet, "/login", nil)
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	c.SetPath("/login")
+
+	if assert.NoError(t, Login(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+func TestRegister(t *testing.T) {
+	e := echo.New()
+	SetRenderer(e)
+
+	req := httptest.NewRequest(http.MethodGet, "/register", nil)
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	c.SetPath("/register")
+
+	if assert.NoError(t, Register(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+func TestRegisterPost(t *testing.T) {
+	e := echo.New()
+	SetRenderer(e)
+
+	req := httptest.NewRequest(http.MethodPost, "/register", strings.NewReader(mockUser))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	c.SetPath("/register")
+
+	if assert.NoError(t, RegisterPost(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, mockUserReturn, rec.Body.String())
 	}
 }
